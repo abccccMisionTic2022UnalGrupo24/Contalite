1e01e2bb66ad9c5e2b2f95d71f311f4fc14ccfd7
Camartinezm92 <Camartinezm92@gmail.com>
2022-09-09 08:05:01 -0500

    no me deja hacer nada

--- Contalite/gestiorTransaccional/admin.py
+++ Contalite/gestiorTransaccional/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+<<<<<<< Updated upstream
+=======
+# Register your models here.
+>>>>>>> Stashed changes
 from gestiorTransaccional.models.empleado import Empleado
 from gestiorTransaccional.models.empresa import Empresa
 from gestiorTransaccional.models.transaccion import Transaccion
@@ -8,4 +12,10 @@ from gestiorTransaccional.models.user import User
 admin.site.register(Empleado)
 admin.site.register(Empresa)
 admin.site.register(Transaccion)
+<<<<<<< Updated upstream
 admin.site.register(User)
+=======
+admin.site.register(User)
+
+# Register your models here.
+>>>>>>> Stashed changes
