e9d4c9d18ea2405ece230fabcc4658360bec38c7
carlosesuescun7506 <carlosesuescun1706@gmail.com>
2022-09-15 22:29:16 -0500

    codigo para presentacion sprint 2

--- Contalite/gestiorTransaccional/views/empleado.py
+++ Contalite/gestiorTransaccional/views/empleado.py
@@ -30,20 +30,20 @@ class EmpleadoView(View):
                 datos={'mensaje':'no se encontraron empleados'}
             return JsonResponse(datos) 
 
-    def post(self, request):
+    def post(self, request):# agregar
         data=json.loads(request.body)
         crear_emp = Empleado(id_user_id=data['id_user_id'],nom_emp=data['nom_emp'],ape_emp=data['ape_emp'],email=data['email'],telefono=data['telefono'],empresa_id=data['empresa_id'],fecha_creacion=data['fecha_creacion'])
         crear_emp.save()
         datos={'mensaje':'empleado registrado exitosamente'}
         return JsonResponse(datos)   
 
-    def put(self, request, id_empleado):
+    def put(self, request, id_empleado): # modificar
         data = json.loads(request.body)
         emp= list(Empleado.objects.filter(id_empleado=id_empleado).values())
         if len(emp)>0:
             m_empleado=Empleado.objects.get(id_empleado=id_empleado)
             #m_empleado.id_empleado=data["id_empleado"]
-            m_empleado.id_user=data["id_user"]
+            #m_empleado.id_user=data["id_user"]
             m_empleado.nom_emp=data["nom_emp"]
             m_empleado.ape_emp=data["ape_emp"]
             m_empleado.email=data["email"]
